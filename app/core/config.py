670dfe24e56f68e3611baff90bc9234e41fe48b6
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-05 15:21:41 -0500

    Refactor config.py to retrieve BACKEND_CORS_ORIGINS from environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 class Settings:
     GOOGLE_API_KEY: str = os.environ.get("GOOGLE_API_KEY")
+    BACKEND_CORS_ORIGINS = os.environ.get("BACKEND_CORS_ORIGINS")
 
 settings = Settings()
