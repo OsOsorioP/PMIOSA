a192dd18d57627cf400803eba39533f4b9264f54
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-08 15:28:53 -0500

    Refactor CORS middleware configuration to use static origins in main.py

--- app/main.py
+++ app/main.py
@@ -9,22 +9,20 @@ app_fastapi = FastAPI(
     version="0.2.0"
 )
 
-if settings.BACKEND_CORS_ORIGINS:
-    app_fastapi.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+settings
 
+origins = ["http://localhost:5173",]
+
+app_fastapi.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True, 
+    allow_methods=["*"],    
+    allow_headers=["*"],    
+)
 
 app_fastapi.include_router(router, prefix="/api/v1") 
 
-# Endpoint de bienvenida en la raíz de la app (diferente al del router)
 @app_fastapi.get("/")
 async def app_root():
     return {"message": "Bienvenido a la Plataforma Multiagente de Agricultura Sostenible"}
-
-# Para ejecutar (desde el directorio raíz 'agricultura_multiagente_api/'):
-# uvicorn app.main:app_fastapi --reload
